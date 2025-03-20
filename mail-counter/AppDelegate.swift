9dd414eaa98265aeee5a09b2348aa2f2edabd6d3
Shigeru Hagiwara <hgwrsgr@gmail.com>
2025-03-20 16:55:42 +0900

    Initial Commit

---
+++ mail-counter/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  mail-counter
+//
+//  Created by Shigeru Hagiwara on 2025/03/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ mail-counterTests/mail_counterTests.swift
@@ -0,0 +1,17 @@
+//
+//  mail_counterTests.swift
+//  mail-counterTests
+//
+//  Created by Shigeru Hagiwara on 2025/03/20.
+//
+
+import Testing
+@testable import mail_counter
+
+struct mail_counterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
